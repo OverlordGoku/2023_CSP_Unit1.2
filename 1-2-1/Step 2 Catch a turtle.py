b76ce9b2f56d0c8cd6be153dd1db010181c3c65e
12862 <daniel.gosses@vtsd.com>
2023-11-08 09:18:27 -0500

    Hello World

---
+++ 1-2-1/Step 2 Catch a turtle.py
@@ -0,0 +1,16 @@
+# a121_catch_a_turtle.py
+#-----import statements-----
+import turtle as trtl
+tri = trtl.Turtle
+
+#-----game configuration----
+tri_color = "red"
+#-----initialize turtle-----
+tri = trtl.Turtle()
+tri.color(tri_color)
+tri.shape("triangle")
+tri.shapesize(26)
+#-----game functions--------
+
+
+#-----events----------------
