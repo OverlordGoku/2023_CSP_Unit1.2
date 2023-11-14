9feef21b04bf80d33e6181ff8eafdf75cde7cf33
12862 <daniel.gosses@vtsd.com>
2023-11-14 07:52:43 -0500

    Hello World

--- 1-2-1/Step 2 Catch a turtle.py
+++ 1-2-1/Step 2 Catch a turtle.py
@@ -1,19 +1,28 @@
 # a121_catch_a_turtle.py
 #-----import statements-----
 import turtle as trtl
+import random as rand
 tri = trtl.Turtle
 wn = trtl.Screen()
+
 #-----game configuration----
 tri_color = "red"
 #-----initialize turtle-----
 tri = trtl.Turtle()
 tri.color(tri_color)
 tri.shape("triangle")
-tri.shapesize(24)
-tri.fillcolor()
+tri.shapesize(10)
+
 #-----game functions--------
+def tri_clicked(x,y):
+    print("tri_clicked!")
+    change_position()
 
-
+def change_position():
+    x = rand.randint(1,400)
+    y = rand.randint(1,300)
+    tri.goto(x,y)
 #-----events----------------
+tri.onclick(tri_clicked)
 trtl.Screen()
 wn.mainloop()
