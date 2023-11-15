f96c8b4c68d8ee35682f0002fa883d76c49ae9ea
12862 <daniel.gosses@vtsd.com>
2023-11-15 10:08:14 -0500

    Hello World

---
+++ 1-2-1/Asterix Quiz.py
@@ -0,0 +1,11 @@
+Starlvl = ("*")
+SpcStar = ("*")
+
+          def star():
+        for level in range(5):
+        print(Starlvl)
+        Starlvl = Starlvl + ("*")
+        for spacing in range(3):
+        Starlevel = SpcStar + Starlvl
+
+star()
