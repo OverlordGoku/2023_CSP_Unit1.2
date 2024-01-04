95863b65e4f73cc2b16a27172ba52785c540bf9e
12862 <daniel.gosses@vtsd.com>
2024-01-04 09:20:05 -0500

    Lab 1

---
+++ Parameters/Example1.py
@@ -0,0 +1,17 @@
+x = 50
+y = 4
+z = 42
+
+# Write a function called add_vals() that adds
+# Write the function so that if num1 is bigger than num2
+# it displays num3, otherwise it says invalid input
+
+def add_vals(num1, num2, num3):
+
+if num1 > num2:
+    print(num3)
+
+else:
+    print("Invalid input.")
+
+add_vals(3,  4,  7)
