3ebf0ede0b8ce95912b61a9b08240c76955f943a
12862 <daniel.gosses@vtsd.com>
2024-01-05 08:26:19 -0500

    Lab 1

---
+++ 1.2.5/the worlds hardest game.py
@@ -0,0 +1,24 @@
+import turtle as trtl
+
+runner = trtl.Turtle
+
+runner.forward(100)
+def up():
+    if (wn.onkeypress("a")):
+        runner.forward(10)
+
+
+    up()
+
+
+
+
+
+
+
+
+
+
+wn = trtl.Screen()
+wn.mainloop()
+
