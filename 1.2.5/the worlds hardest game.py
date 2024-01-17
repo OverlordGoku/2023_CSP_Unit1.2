a737344e754455f655e678a2aa147132af5b7397
12862 <daniel.gosses@vtsd.com>
2024-01-17 11:07:09 -0500

    Lab 1

--- 1.2.5/the worlds hardest game.py
+++ 1.2.5/the worlds hardest game.py
@@ -1,19 +1,30 @@
 import turtle as trtl
 wn = trtl.Screen()
-runner = trtl.Turtle
-box = trtl.Turtle
+runner = trtl.Turtle()
+box = trtl.Turtle()
 wn.bgpic("bg2.gif")
 
 
 runner.penup()
 
-#box turtle setup
-box.shape(box="square")
+#turtle position setup
+runner.right(180)
+runner.forward(420)
+runner.right(180)
+
+#box setup
+box.shape("square")
+box.color("red")
+box.shapesize(5)
+box.left(180)
+box.forward(100)
+box.right(90)
+box.forward(130)
 
 
 #keypress
 def w():
-    runner.forward(45)
+        runner.forward(45)
 
 def s():
     runner.back(45)
@@ -24,6 +35,11 @@ def a():
 def d():
     runner.right(45)
 
+
+#box movement
+for movement in range(10000):
+    box.right(180)
+    box.forward(300)
 
 
 #listen for keypress
