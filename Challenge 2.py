b8d93d9eae9f4a72e2989716f37d964ada4bd562
12862 <daniel.gosses@vtsd.com>
2024-01-25 08:41:29 -0500

    Hello World

---
+++ Challenge 2.py
@@ -0,0 +1,14 @@
+#Write a method called addSubtract()
+#This method should take a parameter called operation
+#When the method is called, ask the user for two numbers
+#Do the operation of the parameter to the inputs
+
+def addSubtract(operation):
+    num1 = input("What is the first number?")
+    num2 = int(input("What is"))
+If nums
+
+
+
+addSubtract("add")
+addSubtract("subtract")
